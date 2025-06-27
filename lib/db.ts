79aeb672f4d6a1d7ba3ad115e1fde5cb12ea4a06
Sean O'Reilly <seansoreilly@gmail.com>
2025-06-27 19:01:27 +1000

    Neon Postgres Database & Configuration

---
+++ lib/db.ts
@@ -0,0 +1,17 @@
+import { neon } from '@neondatabase/serverless';
+
+export const db = neon(process.env.DATABASE_URL!);
+
+export type Conversation = { 
+  id: string; 
+  guid: string; 
+  created_at: string 
+};
+
+export type Message = { 
+  id: string; 
+  conversation_id: string; 
+  role: string; 
+  content: string; 
+  created_at: string 
+};
